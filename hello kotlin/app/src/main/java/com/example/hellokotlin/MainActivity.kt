6a671e0ee89ab67b2f11e5dad8fb56b6a531a0a8
Emranul Hoque <122172406+emranrakib62@users.noreply.github.com>
2024-10-10 20:55:05 +0600

    Add files via upload

---
+++ hello kotlin/app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hellokotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    val button=findViewById<Button>(R.id.button)
+    val tvhello=findViewById<TextView>(R.id.tvdisplay)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+button.setOnClickListener {
+    tvhello.text="hello from kotlin! welcome for android developer"
+
+}
+
+    }
+}
